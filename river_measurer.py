7f6eed1b9061ff117c07604dd7385dbba1b1b2eb
Adoria298 <bananaorange29@outlook.com>
2018-08-23 13:10:01 +0100

    Added error handling/status messages.

--- river_measurer.py
+++ river_measurer.py
@@ -8,10 +8,12 @@ def get_json_data(json_url):
     formats it to remove government extras ["@context",  "meta", "@id", 
     "measure"].
     """
+    print("Getting data from the web... ")
     #download file and reads into program as variable data
     with urllib.request.urlopen(json_url) as url:
         data = json.loads(url.read().decode())
 
+    print("Formating data... ")
     #deletes unnecessary info
     del data["@context"]
     del data["meta"]
@@ -31,15 +33,12 @@ def convert_to_csv(output_file, data):
     #used twice
     fieldnames = ["dateTime", "value"]
 
-
     # double checks and appends existing data
     if os.path.exists(output_file): 
+        print("Comparing data to already existing data... ")
         with open(output_file, mode='r') as csv_file:
             existing_data = list(csv.DictReader(csv_file))
             #its a list of dicts, not a dict
-            print(existing_data)
-            print(type(existing_data))
-
             #check
             line_count = 0
             for erow in existing_data:
@@ -54,6 +53,7 @@ def convert_to_csv(output_file, data):
                 
 
     with open(output_file, mode="w") as csv_file:
+        print(f"Saving data to {output_file}... ")
         #prep work
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
@@ -62,10 +62,17 @@ def convert_to_csv(output_file, data):
         writer.writerows(data["items"])
       
     
-def main():              
-    data = get_json_data("https://environment.data.gov.uk/flood-monitoring/id"
-    + "/measures/724629-level-stage-i-15_min-m/readings?_sorted&_limit=500")
-    convert_to_csv("caton_readings.csv", data)
+def main(): 
+    try:            
+        data = get_json_data("https://environment.data.gov.uk/flood-monitoring/id"
+        + "/measures/724629-level-stage-i-15_min-m/readings?_sorted&_limit=500")
+        convert_tof_csv("caton_readings.csv", data)
+        print("Finished.")
+    except Exception as e:
+        print("An error occurred; it is described below.")
+        print(e)
+    finally: 
+        input("Press enter to exit.")
 
 if __name__ == "__main__":
     main()
